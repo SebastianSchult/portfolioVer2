dd4b934c99b69b8307e00c97e3b505336bc3c2f0
SebastianSchult <155525668+SebastianSchult@users.noreply.github.com>
2024-11-06 11:28:42 +0100

    adding portfolio-section

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { LandingPageComponent } from "./landing-page/landing-page.component";
 import { AboutMeComponent } from "./about-me/about-me.component";
 import { SkillsComponent } from "./skills/skills.component";
+import { PortfolioSectionComponent } from "./portfolio-section/portfolio-section.component";
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [LandingPageComponent, AboutMeComponent, SkillsComponent],
+  imports: [LandingPageComponent, AboutMeComponent, SkillsComponent, PortfolioSectionComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
