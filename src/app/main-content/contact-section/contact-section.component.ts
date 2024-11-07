191dcbb6e6d14ca4e70d03e9322c734f7d9d6e52
SebastianSchult <155525668+SebastianSchult@users.noreply.github.com>
2024-11-07 10:22:26 +0100

    adding contact-section adding form

--- src/app/main-content/contact-section/contact-section.component.ts
+++ src/app/main-content/contact-section/contact-section.component.ts
@@ -1,12 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-contact-section',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './contact-section.component.html',
   styleUrl: './contact-section.component.scss'
 })
 export class ContactSectionComponent {
 
+  contactData = {
+    name: "",
+    email: "",
+    message: "",
+  }
+
+onSubmit() {
+  console.log(this.contactData)
+}
 }
