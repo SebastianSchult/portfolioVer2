f9f4c50745cbc2d7192e316e6c195eb2aeff67e4
SebastianSchult <155525668+SebastianSchult@users.noreply.github.com>
2024-11-14 16:49:29 +0100

    response navbar - landing-page

--- src/app/shared/nav-bar/nav-bar.component.ts
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
 
+  showResponsiveMenu = false;
+
+  openResponsiveMenu() {
+    this.showResponsiveMenu = !this.showResponsiveMenu;
+  }
+  
+  closeResponsiveMenu(){
+    this.showResponsiveMenu = false;
+  
+  }
+
 }
+
+
