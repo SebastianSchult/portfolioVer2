aa316a17989777ecd6e94ef37c1eda76ee609a42
SebastianSchult <155525668+SebastianSchult@users.noreply.github.com>
2024-12-05 17:06:34 +0100

    translateservice navbar about-me-section

--- src/app/main-content/about-me/about-me.component.ts
+++ src/app/main-content/about-me/about-me.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { LanguageService } from '../../serices/language.service';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
 export class AboutMeComponent {
 
+  constructor(public languageService: LanguageService){}
+
+  changeLanguage(language: string) {
+    this.languageService.changeLanguage(language);
+  }
 }
