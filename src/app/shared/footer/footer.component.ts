af635d9138fd5d0bc280a9991846cb2a56b7cec1
SebastianSchult <155525668+SebastianSchult@users.noreply.github.com>
2024-12-05 19:48:55 +0100

    translate service footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { LanguageService } from '../../serices/language.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+
+  constructor(public languageService: LanguageService) {}
+
+  changeLanguage(language: string) {
+    this.languageService.changeLanguage(language);
+  }
 
 }
