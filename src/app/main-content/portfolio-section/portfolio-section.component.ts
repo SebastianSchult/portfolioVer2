53d769f7f09b74be87a0620938d7667cf8cedb27
SebastianSchult <155525668+SebastianSchult@users.noreply.github.com>
2024-12-05 18:57:32 +0100

    translate-service portfolio section

--- src/app/main-content/portfolio-section/portfolio-section.component.ts
+++ src/app/main-content/portfolio-section/portfolio-section.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { LanguageService } from '../../serices/language.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-portfolio-section',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './portfolio-section.component.html',
   styleUrl: './portfolio-section.component.scss'
 })
 export class PortfolioSectionComponent {
+  constructor(public languageService: LanguageService) {}
+
+  changeLanguage(language: string) {
+    this.languageService.changeLanguage(language);
+  }
+
 
 }
