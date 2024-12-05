6ce98477d4310d88ab2a3c77d9bac9764ed34350
SebastianSchult <155525668+SebastianSchult@users.noreply.github.com>
2024-12-05 18:32:20 +0100

    fix scrolldown arrow

--- src/app/main-content/landing-page/landing-page.component.ts
+++ src/app/main-content/landing-page/landing-page.component.ts
@@ -1,20 +1,24 @@
 import { Component } from '@angular/core';
 import { LanguageService } from '../../serices/language.service';
 
-
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   imports: [],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss'
+  styleUrl: './landing-page.component.scss',
 })
 export class LandingPageComponent {
-
-  constructor(public languageService: LanguageService){}
+  constructor(public languageService: LanguageService) {}
 
   changeLanguage(language: string) {
     this.languageService.changeLanguage(language);
   }
 
+  scrollToContact() {
+    const contactSection = document.querySelector('#contact');
+    if (contactSection) {
+      contactSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
